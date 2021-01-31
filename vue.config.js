374bd34190a1704f9cd8183a7e767fd6d2e8000b
CP <1599242486@qq.com>
2021-01-31 23:28:27 +0800

    '工具'

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+// const webpack = require("webpack");
+
 module.exports = {
   devServer: {
     port: 8888, // 端口
     open: true // 自动打开浏览器
+  },
+  configureWebpack: {
+    plugins: [
+      // new webpack.DllReferencePlugin({
+      //   context: process.cwd(),
+      //   manifest: require("./public/vendor/vendor-manifest.json")
+      // })
+    ]
   }
 };
